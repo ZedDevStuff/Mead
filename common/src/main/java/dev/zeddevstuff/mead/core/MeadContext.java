01fd03607d45a0bf62ef5c008bee5f2fa5385b23
ZedDevStuff <kouamejuniorauguste@gmail.com>
2025-07-30 07:46:32 +0000

    Listing stylesheets in mod for preloading

--- common/src/main/java/dev/zeddevstuff/mead/core/MeadContext.java
+++ common/src/main/java/dev/zeddevstuff/mead/core/MeadContext.java
@@ -10,12 +10,16 @@ import dev.zeddevstuff.mead.core.elements.parsing.StyleElement;
 import dev.zeddevstuff.mead.parsing.MeadParser;
 import dev.zeddevstuff.mead.parsing.MeadStyleSheetsParser;
 import dev.zeddevstuff.mead.styling.*;
+import dev.zeddevstuff.mead.utils.NullUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 
 import java.io.InputStream;
+import java.security.CodeSource;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 
 /**
  * The centralized interface to Mead. It is recommended for you to use this instead of directly using Mead classes.
@@ -32,6 +36,7 @@ public class MeadContext
     {
         this.modid = modid;
         registerDefaults();
+        loadStyleSheets();
     }
 
     public MeadParser createParser()
@@ -73,8 +78,45 @@ public class MeadContext
     {
         MeadStyle style = null;
     }
+    private static Pattern tempPattern = Pattern.compile(".+\\.jar");
     public void loadStyleSheets()
     {
-        // TODO: Find a way to load all style sheets in the current mod's resources.
+        try
+        {
+            var className = Thread.currentThread().getStackTrace()[3].getClassName();
+            Class<?> clazz = Class.forName(className);
+            NullUtils.ifNotNull(clazz.getProtectionDomain().getCodeSource(), codeSource ->
+                {
+                    NullUtils.ifNotNull(codeSource.getLocation(), location -> {
+                        var matcher = tempPattern.matcher(location.getPath());
+                        if(matcher.find())
+                        {
+                            try (JarFile jarFile = new JarFile(matcher.group()))
+                            {
+                                jarFile.stream()
+                                    .filter(entry -> entry.getName().startsWith("assets/" + modid + "/ui/") && entry.getName().endsWith(".mss"))
+                                    .forEach(entry -> {
+                                        try (InputStream inputStream = jarFile.getInputStream(entry))
+                                        {
+                                            String content = new String(inputStream.readAllBytes());
+                                            // TODO: Parse the Mead stylesheet and register it
+                                        } catch (Exception e)
+                                        {
+                                            e.printStackTrace();
+                                        }
+                                    });
+                            }
+                            catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                });
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Loading Mead stylesheets for mod: " + modid);
     }
 }
